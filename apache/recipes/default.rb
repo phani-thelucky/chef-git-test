d972fe3c4c5fa22f46f8a87f8813b16bc42910e3
phani-thelucky <phani.thelucky@gmail.com>
2016-06-25 17:24:36 -0400

    apache cookbook

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -6,13 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+document = "/var/www/html"
+
 package "apache2"
 
 service "apache2"  do
 	action [:enable, :start]
 end
 
-cookbook_file "/var/www/index.html" do
-	source "index.html"
+#cookbook_file "/var/www/index.html" do
+#	source "index.html"
+#	mode "0644"
+#end
+
+template "#{document}/index.html" do
+	source "index.html.erb"
 	mode "0644"
 end
